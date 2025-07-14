e99abc572d354ed3c9a8c38e7ecd5aa5c6960bea
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-07-14 11:34:43 +0100

    updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import UseLenis from "./hooks/useLenis";
 export const metadata: Metadata = {
   title: "McApline",
   icons: {
@@ -27,10 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={` antialiased`}>
+        <UseLenis>{children}</UseLenis>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -35,17 +35,7 @@ export default function Home() {
     });
     return () => context.revert();
   }, []);
-  useEffect(() => {
-    const lenis = new Lenis();
-    function raf(time: any) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+
   return (
     <div className="ThinCanel">
       <SectionOne />
